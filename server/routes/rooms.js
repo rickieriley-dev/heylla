2e971a1f02f855489b31c51c98f6f7de8db66518
rickieriley-dev <rickieriley05@gmail.com>
2026-03-13 23:18:38 +0800

    Update rooms.js

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { getRooms, createRoom, getRoom } = require('../controllers/roomController');
+const { getRooms, createRoom, getRoom, getMyRoom, deleteRoom } = require('../controllers/roomController');
 const auth = require('../middleware/auth');
 
 router.get('/', getRooms);
 router.post('/', auth, createRoom);
+router.get('/mine', auth, getMyRoom);  // must be before /:id
 router.get('/:id', getRoom);
+router.delete('/:id', auth, deleteRoom);
 
 module.exports = router;
