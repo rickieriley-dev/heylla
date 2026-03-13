3141cdb9aa7fc0ebc536de41523382811f66c386
rickieriley-dev <rickieriley05@gmail.com>
2026-03-14 00:04:40 +0800

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { getUser, updateUser } = require('../controllers/userController');
+const { getUser, updateUser, followUser, checkFollow, getFollowingRooms } = require('../controllers/userController');
 const auth = require('../middleware/auth');
- 
+
+router.get('/following-rooms', auth, getFollowingRooms);
 router.get('/:id', getUser);
 router.put('/me', auth, updateUser);
- 
+router.post('/:id/follow', auth, followUser);
+router.get('/:id/follow', auth, checkFollow);
+
 module.exports = router;
- 
